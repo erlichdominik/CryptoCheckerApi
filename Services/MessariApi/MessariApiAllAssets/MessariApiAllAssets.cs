829fd1a8916ef9823140feadf32deb2ebc80e259
erlichdominik <mimikdesu@gmail.com>
2021-10-28 23:40:50 +0200

    implement GetMessariApiAllAsync

--- Services/MessariApi/MessariApiAllAssets/MessariApiAllAssets.cs
+++ Services/MessariApi/MessariApiAllAssets/MessariApiAllAssets.cs
@@ -1,4 +1,5 @@
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using crypto_checker_api.Models.MessariApiModels.MessariAllAssets;
 
@@ -7,15 +8,19 @@ namespace crypto_checker_api.Services.MessariApi.MessariApiAllAssets
     public class MessariApiAllAssets : IMessariApiAllAssets
     {
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly string _messariApiAllAssetsCallUri = "https://data.messari.io/api/v2/assets";
 
         public MessariApiAllAssets(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
 
-        public Task<MessariAllAssets> GetMessariApiAllAssetsAsync()
+        public async Task<MessariAllAssets> GetMessariApiAllAssetsAsync()
         {
-            throw new System.NotImplementedException();
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetFromJsonAsync<MessariAllAssets>(_messariApiAllAssetsCallUri);
+
+            return response;
         }
     }
 }
