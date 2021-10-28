d91bab65b56ebe593506f1649730235076a39bdf
erlichdominik <mimikdesu@gmail.com>
2021-10-28 23:35:31 +0200

    add constructor

--- Services/MessariApi/MessariApiAllAssets/MessariApiAllAssets.cs
+++ Services/MessariApi/MessariApiAllAssets/MessariApiAllAssets.cs
@@ -1,3 +1,4 @@
+using System.Net.Http;
 using System.Threading.Tasks;
 using crypto_checker_api.Models.MessariApiModels.MessariAllAssets;
 
@@ -5,6 +6,13 @@ namespace crypto_checker_api.Services.MessariApi.MessariApiAllAssets
 {
     public class MessariApiAllAssets : IMessariApiAllAssets
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MessariAllAssets(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public Task<MessariAllAssets> GetMessariApiAllAssetsAsync()
         {
             throw new System.NotImplementedException();
