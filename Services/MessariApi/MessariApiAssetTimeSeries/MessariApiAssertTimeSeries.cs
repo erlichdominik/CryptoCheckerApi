9822f5eeae3622ed2620dd1c5c974879a0cb3b01
erlichdominik <mimikdesu@gmail.com>
2021-10-28 23:49:01 +0200

    implement GetMEssariAssetTimeSeriesAsync

--- Services/MessariApi/MessariApiAssetTimeSeries/MessariApiAssertTimeSeries.cs
+++ Services/MessariApi/MessariApiAssetTimeSeries/MessariApiAssertTimeSeries.cs
@@ -1,3 +1,5 @@
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using crypto_checker_api.Models.MessariApiModels.MessariAssetTimeSeries;
 
@@ -5,9 +7,20 @@ namespace crypto_checker_api.Services.MessariApi.MessariApiAssetTimeSeries
 {
     public class MessariApiAssetTimeSeries : IMessariApiAssertTimeSeries
     {
-        public Task<MessariAssetTimeSeries> GetMessariAssetTimeSeriesAsync(string slug)
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MessariApiAssetTimeSeries(IHttpClientFactory httpClientFactory)
         {
-            throw new System.NotImplementedException();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<MessariAssetTimeSeries> GetMessariAssetTimeSeriesAsync(string slug)
+        {
+            //TODO optional add another options to search time series
+            var client = _httpClientFactory.CreateClient();
+            string callUri = $"https://data.messari.io/api/v1/assets/{slug}/metrics/price/time-series?start=2020-01-01&end=2020-02-01&interval=1d";
+            var response = await client.GetFromJsonAsync<MessariAssetTimeSeries>(callUri);
+            return response;
         }
     }
 }
