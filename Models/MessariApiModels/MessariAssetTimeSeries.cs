9a53ff1db0690195a6046d0b6947403d8c9eb770
erlichdominik <mimikdesu@gmail.com>
2021-10-28 22:47:25 +0200

    format MessariAssetTimeSeries.cs

--- Models/MessariApiModels/MessariAssetTimeSeries.cs
+++ Models/MessariApiModels/MessariAssetTimeSeries.cs
@@ -2,7 +2,8 @@ using System;
 using System.Text.Json.Serialization;
 using System.Collections.Generic;
 
-namespace crypto_checker_api.Models.MessariApiModels {
+namespace crypto_checker_api.Models.MessariApiModels
+{
     public class Status
     {
         [JsonPropertyName("elapsed")]
