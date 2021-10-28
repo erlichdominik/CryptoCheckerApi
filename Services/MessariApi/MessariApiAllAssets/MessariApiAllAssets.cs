8d7b69a840e73b762d148d38866bbf2385d421d4
erlichdominik <mimikdesu@gmail.com>
2021-10-28 23:30:13 +0200

    implement GetMessariApiAllAsync

--- Services/MessariApi/MessariApiAllAssets/MessariApiAllAssets.cs
+++ Services/MessariApi/MessariApiAllAssets/MessariApiAllAssets.cs
@@ -1,8 +1,14 @@
+using System.Threading.Tasks;
+using crypto_checker_api.Models.MessariApiModels.MessariAllAssets;
 
 namespace crypto_checker_api.Services.MessariApi.MessariApiAllAssets
 {
-    public class MessariApiAllAssets
+    public class MessariApiAllAssets : IMessariApiAllAssets
     {
+        public Task<MessariAllAssets> GetMessariApiAllAssetsAsync()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
 
