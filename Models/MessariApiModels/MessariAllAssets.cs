dd732d8a7ec30ac66330f6655aa18a76ea079d0a
erlichdominik <mimikdesu@gmail.com>
2021-10-28 23:13:10 +0200

    fix namespace

--- Models/MessariApiModels/MessariAllAssets.cs
+++ Models/MessariApiModels/MessariAllAssets.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace crypto_checker_api.Models.MessariApiModels
+namespace crypto_checker_api.Models.MessariApiModels.MessariAllAssets
 {
 
     public class Status
