33fec5c00947d458c7df13b87c6764a3428fe08a
erlichdominik <mimikdesu@gmail.com>
2021-11-03 17:20:24 +0100

    add GetMessariAllAssetsWithMarketDataAsync

--- Services/MessariApi/MessariApiAllAssets/IMessariApiAllAssets.cs
+++ Services/MessariApi/MessariApiAllAssets/IMessariApiAllAssets.cs
@@ -5,5 +5,7 @@ namespace crypto_checker_api.Services.MessariApi.MessariApiAllAssets
     public interface IMessariApiAllAssets
     {
         Task<crypto_checker_api.Models.MessariApiModels.MessariAllAssets.MessariAllAssets> GetMessariApiAllAssetsAsync();
+        Task<crypto_checker_api.Models.MessariApiModels.MessariAllAssetsMarketData.MessariAllAssetsMarketData> GetMessariAllAssetsWithMarketDataAsync();
     }
+
 }
--- Services/MessariApi/MessariApiAllAssets/MessariApiAllAssets.cs
+++ Services/MessariApi/MessariApiAllAssets/MessariApiAllAssets.cs
@@ -2,23 +2,38 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using crypto_checker_api.Models.MessariApiModels.MessariAllAssets;
+using crypto_checker_api.Models.MessariApiModels.MessariAllAssetsMarketData;
 
 namespace crypto_checker_api.Services.MessariApi.MessariApiAllAssets
 {
     public class MessariApiAllAssets : IMessariApiAllAssets
     {
         private readonly IHttpClientFactory _httpClientFactory;
-        private readonly string _messariApiAllAssetsCallUri = "https://data.messari.io/api/v2/assets";
 
         public MessariApiAllAssets(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
 
+        public async Task<MessariAllAssetsMarketData> GetMessariAllAssetsWithMarketDataAsync()
+        {
+            string callUri = "https://data.messari.io/api/v2/assets?fields=id,slug,symbol,metrics/market_data";
+            return await GetDataFromJsonAsync<MessariAllAssetsMarketData>(callUri);
+        }
+
         public async Task<MessariAllAssets> GetMessariApiAllAssetsAsync()
         {
+
+            string callUri = "https://data.messari.io/api/v2/assets";
+
+            return await GetDataFromJsonAsync<MessariAllAssets>(callUri);
+        }
+
+
+        private async Task<T> GetDataFromJsonAsync<T>(string callUri)
+        {
             var client = _httpClientFactory.CreateClient();
-            var response = await client.GetFromJsonAsync<MessariAllAssets>(_messariApiAllAssetsCallUri);
+            var response = await client.GetFromJsonAsync<T>(callUri);
 
             return response;
         }
